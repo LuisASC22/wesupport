dcee06198bea91ee2e943d6dcbfd4ae9e81393fe
LuisASC22 <109644081+LuisASC22@users.noreply.github.com>
2022-08-09 16:16:44 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+/*http://127.0.0.1:5500/#
+
 /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
 particlesJS.load('particles-js', 'particles.json', function() {
     console.log('callback - particles.js config loaded');
